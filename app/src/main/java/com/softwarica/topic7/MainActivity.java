9a9105640b11089b519a6ea8a32f3d49b102913d
Sabina Sunuwar <sabinasunuwar4@gmail.com>
2019-12-18 12:30:07 +0545

    Dictionary App Added.

--- app/src/main/java/com/softwarica/topic7/MainActivity.java
+++ app/src/main/java/com/softwarica/topic7/MainActivity.java
@@ -2,13 +2,47 @@ package com.softwarica.topic7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAddWord, btnDisplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddWord = findViewById(R.id.btnAddWord);
+        btnDisplay = findViewById(R.id.btnDisplay);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowMeaningActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAddWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddWordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
